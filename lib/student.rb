e3f88f5c9e5d02eca778e302d18c74d1cadae15e
Patrick Harrison <jahman@bellatlantic.net>
2019-10-03 12:55:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,8 +39,9 @@ class Student
     end
   end
 
-def self.create(name:, grade:)
-    student = Student.new(name, grade)
+
+    def self.create(name, grade)
+    student = self.new(name, grade)
     student.save
     student
   end
